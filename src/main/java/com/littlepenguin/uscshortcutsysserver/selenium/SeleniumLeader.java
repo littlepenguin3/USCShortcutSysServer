0cc0e7943e7f83139209bf4793f3bc132bf0e654
littlePenguin <3812256@qq.com>
2022-05-04 23:44:41 +0800

    selenium包下 多线程架构

---
+++ src/main/java/com/littlepenguin/uscshortcutsysserver/selenium/SeleniumLeader.java
@@ -0,0 +1,16 @@
+package com.littlepenguin.uscshortcutsysserver.selenium;
+
+/**
+ * @author littlepenguin
+ * @date 2022/05/04
+ * @version 1.0
+ * selenium.SeleniumLeader 领导类负责领导SeleniumGroup
+ */
+public class SeleniumLeader {
+    /**
+     * lead方法进行领导WorkerGroup进行工作
+     */
+    public void lead(){
+
+    }
+}
